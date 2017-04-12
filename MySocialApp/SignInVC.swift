1f8cf02dd9cb4d4ef5e500b33c3a9be9a4394c01
Hyperactive5 <bonderadi@gmail.com>
2017-04-12 16:22:37 +0300

    First commit

---
+++ MySocialApp/SignInVC.swift
@@ -0,0 +1,20 @@
+//
+//  SignInVC.swift
+//  MySocialApp
+//
+//  Created by Hyperactive5 on 12/04/2017.
+//  Copyright © 2017 Hyperactive5. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+
+
+}
+
